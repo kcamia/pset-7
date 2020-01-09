f3b53f3cf33144dc000779dabd45cdc2737faf03
Kristina Camia <kcamia19@gmail.com>
2020-01-09 12:56:50 -0500

    commonEnd complete 2

--- src/exercises.js
+++ src/exercises.js
@@ -1,8 +1,15 @@
 function commonEnd(a, b) {
   if (!a || a.length === 0 || !b || b.length === 0) {
     return false;
-  } 
-
+  } else {
+    let aEnd = a.length - 1;
+    let bEnd = b.length - 1;
+    if (a[0] === b[0] || a[aEnd] === b[bEnd]) {
+      return true;
+    } else {
+      return false;
+    }
+  };
   // write your code here
 }
 
