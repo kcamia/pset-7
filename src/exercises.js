878b394efa7168e858dfdc01a288a9ca4e0cf190
Kristina Camia <kcamia19@gmail.com>
2020-01-15 13:03:55 -0500

    all exercises to max pass except 1, 5

--- src/exercises.js
+++ src/exercises.js
@@ -15,45 +15,42 @@ function commonEnd(a, b) {
 
 function endsMeet(values, n) {
   let empty = [];
-  if (!values || values.length === 0 || n < 0 || values.length < n) {
+  if (!values || values.length < n || n <= 0) {
     return empty;
-  } else if (n === 0 || values.length === 1) {
-    return values;
-//  } else if (n > 0) {
-//    let newArray = [];
-//    for (i = 0; i <= n - 1; i++) {
-//      newArray[i] = values[i];
-//    }
-//    let end;
-//    for (x = values.length - n; x <= values.length; x++) {
-//      end = values[x];
-//      newArray.push(end);
-//    }
-//    return newArray;
+  } else if (n > 0) {
+    let newArray = [];
+    let first;
+    for (f = 0; f <= n - 1; f++) {
+      first = values[f];
+      newArray.push(first);
+    }
+    let end = values.length - 1;
+    let endStart = end - n;
+    for (e = endStart; e <= end; e++) {
+      newArray.push(e);
+    }
+    return newArray;
   } else {
     return values;
   }
+
   // write your code here
 }
 
 function difference(numbers) {
   if (!numbers || numbers.length < 1) {
     return undefined;
-  } else if (numbers.length > 1) {
+  } else if (numbers.length >= 1) {
     for (i = 0; i <= numbers.length - 1; i++) {
       let number = numbers[i];
       if (isNaN(number)) {
         return undefined;
       }
     }
-    if (numbers.length === 1) {
-      return 0;
-    } else {
-      let largest = Math.max.apply(null, numbers);
-      let smallest = Math.min.apply(null, numbers);
-      let difference = largest - smallest;
-      return difference;
-    }
+    let largest = Math.max.apply(null, numbers);
+    let smallest = Math.min.apply(null, numbers);
+    let difference = largest - smallest;
+    return difference;
   } else {
     return undefined;
   }
@@ -61,6 +58,36 @@ function difference(numbers) {
 }
 
 function max(number) {
+  let numeral = true;
+  if (!number || number.length < 3) {
+    return undefined;
+  }
+  let check = number.length % 2;
+  if (check === 0) {
+    return undefined;
+  }
+  if (number.length >= 3) {
+    for (i = 0; i <= number.length - 1; i++) {
+      let unknown = number[i];
+      if (isNaN(unknown)) {
+        numeral = false;
+      }
+    }
+  }
+  if (numeral === false) {
+    return undefined;
+  } else {
+    let end = number.length - 1;
+    let first = number[0];
+    let last = number[end];
+    let middle = number[end / 2];
+    let newArray = [];
+    newArray.push(first);
+    newArray.push(middle);
+    newArray.push(last);
+    let largest = Math.max.apply(null, newArray);
+    return largest;
+    }
   // write your code here
 }
 
