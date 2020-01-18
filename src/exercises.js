d7d18ee0154841196b87bb1e616237138d8937d8
unknown <kcamia19@gmail.com>
2020-01-17 21:46:48 -0800

    idk what this is 11

--- src/exercises.js
+++ src/exercises.js
@@ -14,10 +14,30 @@ function commonEnd(a, b) {
 }
 
 function endsMeet(values, n) {
-  if (!values || values.length === 0 || n < 0) {
-    return ;
+  if (!values || values.length === 0 || n < 0 || n > values.length) {
+    let empty = [];
+    return empty;
   } else if (n === 0) {
-
+    return values;
+  } else if (values.length === 1) {
+    return values;
+  } else if (n > 0) {
+    let firstElements = n - 1;
+    let newArray = [];
+    let first;
+    for (i = 0; i >= firstElements; i--) {
+      first = values[i];
+      newArray.push(first);
+    };
+    let lastElements = values.length - firstElements;
+    let last;
+    for (x = lastElements; x <= values.length; x++) {
+      last = values[x];
+      newArray.push(last);
+    };
+    return newArray;
+  } else {
+    return values;
   }
   // write your code here
 }
