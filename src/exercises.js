ce52ff1e1529cabc61ebbb735b5dc82de7297f3e
unknown <kcamia19@gmail.com>
2020-01-20 08:37:08 -0800

    clumps 15

--- src/exercises.js
+++ src/exercises.js
@@ -253,6 +253,20 @@ function balance(numbers) {
 }
 
 function clumps(values) {
+  let count = 0;
+  if (!values) {
+    return -1;
+  }
+  else {
+    for (let i = 0; i < values.length - 1; i++) {
+    if (values[i - 1] === values[i]) {
+    }
+    else if (values[i] === values[i + 1]) {
+      count++;
+    }
+  }
+  return count;
+  }
   // write your code here
 }
 
